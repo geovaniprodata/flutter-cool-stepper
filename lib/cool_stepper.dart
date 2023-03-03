d279b85163af1726362e3dfbf7b5e505d6cb7d92
Geovani Amaral <geovani.web13@gmail.com>
2023-03-03 13:08:40 -0300

    dfsdsfdf

--- lib/cool_stepper.dart
+++ lib/cool_stepper.dart
@@ -416,17 +416,14 @@ class _CoolStepperState extends State<CoolStepper>
     );
 
     return Column(
-        children: widget.tabRequired
-            ? <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: tabs.map((tab) {
-                    return _buildItem(context, tab.name, listPages[tab.index]);
-                  }).toList(),
-                ),
-                allf,
-                buttons
-              ]
-            : <Widget>[content, allf, buttons]);
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: tabs.map((tab) {
+        return _buildItem(context, tab.name, listPages[tab.index]);
+      }).toList(),
+    );
+    //     allf,
+    //     buttons
+    //   ]
+    // : <Widget>[content, allf, buttons]);
   }
 }
